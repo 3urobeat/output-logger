53d1172d1c1be6cec92993565203bf090e6c53fc
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2021-06-17 19:21:33 +0200

    Added round brackets to empty bracket check, minor code refactor when printing string and fixed empty brackets at the end of the string not getting removed

--- index.js
+++ index.js
@@ -64,17 +64,16 @@ module.exports = function (type, origin, str, nodate, remove) {
     var string = options.msgstructure.replace("type", typestr).replace("origin", originstr).replace("date", date).replace("message", str) //this is shitty code and needs to be changed in the a future version when introducing custom options
 
     //Check for empty brackets and remove them
-    var string = string.replace(/ \| ]/g, "]").replace(/\[ \| /g, "[").replace(/\[\] /g, "") //this probably needs to be updated in a future version to work better for edge cases
+    var string = string.replace(/ \| ]/g, "]").replace(/\[ \| /g, "[").replace(/\[\] /g, "").replace(/ \[\]/g, "") //this probably needs to be updated in a future version to work better for edge cases
+    var string = string.replace(/ \| )/g, ")").replace(/\( \| /g, "(").replace(/\(\) /g, "").replace(/ \(\)/g, "")
 
-    //Print message with remove or without
-    if (remove) {
-        readline.clearLine(process.stdout, 0) //0 clears entire line
-        process.stdout.write(`${string}\r`)
-    } else {
-        readline.clearLine(process.stdout, 0)
-        console.log(`${string}`) 
-    }
-    
+    //Clear line and print message with remove or without
+    readline.clearLine(process.stdout, 0) //0 clears entire line
+
+    if (remove) process.stdout.write(`${string}\r`)
+        else console.log(`${string}`) 
+       
+    //Write message to file
     if (options.outputfile && options.outputfile != "") { //only write to file if the user didn't turn off the feature
         //Remove color codes from the string which we want to write to the text file
         fs.appendFileSync(options.outputfile, string.replace(/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]/g, '') + '\n', err => { //Regex Credit: https://github.com/Filirom1/stripcolorcodes
